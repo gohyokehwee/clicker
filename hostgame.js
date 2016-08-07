0122edb0d4dd1c063ecb636ee41e2d83a7f1aad5
Gabriel Wu <gabrielwu84@gmail.com>
2016-08-08 04:41:52 +0800

    added student states

--- hostgame.js
+++ hostgame.js
@@ -1,5 +1,84 @@
-function studentEngine(studentListDiv){
-    // studentIOEngine
+function lessonEngine(qnStemDiv,studAnsDiv,lessonPlan){
+    var baseJsAdd=
+      "https://gabrielwu84.github.io/clicker-mods/";
+    //var baseJsAdd=
+    //  "http://127.0.0.1/home/proj-clicker/clicker-mods/";
+    var qnNo=-1;
+    var qnObj={};
+    var answeredUuid;
+    this.jsFile;
+    this.jsParams;
+
+    this.nextQn=function(){
+        qnNo++;
+        answeredUuid=[];
+        var qnSpec=lessonPlan[qnNo];
+        var jsFile=baseJsAdd+qnSpec.type+".js";
+        execQn(jsFile,qnSpec.params,qnSpec.stem);
+        navBtnLogic();
+    }
+
+    this.prevQn=function(){
+        qnNo--;
+        answeredUuid=[];
+        var qnSpec=lessonPlan[qnNo];
+        var jsFile=baseJsAdd+qnSpec.type+".js";
+        execQn(jsFile,qnSpec.params,qnSpec.stem);
+        navBtnLogic();
+    }
+    this.resetQn=function(){
+        answeredUuid=[];
+        var qnSpec=lessonPlan[qnNo];
+        var jsFile=baseJsAdd+qnSpec.type+".js";
+        execQn(jsFile,qnSpec.params,qnSpec.stem);
+    }
+
+    this.processAns=function(studentSocketId,studentAns){
+        studentUuid=studentIoObj.socIdToUuid(studentSocketId);
+        if(answeredUuid.indexOf(studentUuid)==-1){
+            answeredUuid.push(studentUuid);
+            qnObj.procAns(studentUuid,studentAns);
+            studentDispObj.markAnswered(studentUuid);
+        } else {
+            console.log(studentUuid+" already answered");
+        }
+    }
+
+    function execQn(jsFile,jsParams,qnStem){
+        lessonObj.jsFile=jsFile; 
+        lessonObj.jsParams=jsParams;
+        qnStemDiv.innerHTML=
+            "<div class='ui-content'>"+qnStem+"</div>";
+        studAnsDiv.innerHTML="";
+        $.getScript(jsFile,function() {
+            qnObj=new qnHostEng(qnStemDiv, studAnsDiv,jsParams);
+            qnStemDiv.innerHTML+=qnObj.stemDisplay();
+            $(qnStemDiv).trigger("create");
+        });
+        var students=studentIoObj.getConnectedStudents();
+        for (var socketId in students){
+            socket.relay(socketId,
+                {"title":"qnCmd","jsFile":jsFile,"params":jsParams}
+            );
+        }
+        studentDispObj.resetAnswered();
+    }
+
+    function navBtnLogic(){
+        if(qnNo==lessonPlan.length-1){
+            ctrlBarObj.hideNextBtn();
+        } else {
+            ctrlBarObj.showNextBtn();
+        }
+        if(qnNo==0){
+            ctrlBarObj.hidePrevBtn();
+        } else {
+            ctrlBarObj.showPrevBtn();
+        }
+    }
+}
+
+function studentIoEngine(){
     var studentList={};
     var socIdToUuid={};
     this.missionObj=null;
@@ -7,14 +86,17 @@ function studentEngine(studentListDiv){
         socIdToUuid[socketId]=uuid;
         if(uuid in studentList){
             studentList[uuid].reConStudent(socketId);
+        	studentDispObj.markConnected(uuid);
         } else {
             var studentObj=new studentClass(socketId,studentName);
             studentList[uuid]=studentObj;
-            studentListDiv.appendChild(studentObj.studentDiv);
+            studentDispObj.addStudent(uuid);
         }
     }
     this.disConStudent=function(socketId){
-        studentList[socIdToUuid[socketId]].disConStudent(socketId);
+    	var uuid=socIdToUuid[socketId];
+		studentList[uuid].disConStudent(socketId);
+        studentDispObj.markDisconnected(uuid);
         delete(socIdToUuid[socketId]);
     }
     this.getConnectedStudents=function(){
@@ -27,12 +109,6 @@ function studentEngine(studentListDiv){
     function studentClass(socketId,studentName){
         this.socketId=socketId;
         this.studentName=studentName;
-        this.studentDiv=function(){
-            var pDiv=document.createElement('button');
-            $(pDiv).addClass("ui-btn ui-corner-all ui-icon-user ui-btn-icon-notext");
-            pDiv.style="float:left; margin:1px 1px; background-size:300%";
-            return pDiv;
-        }();
         this.disConStudent=function(){
             this.socketId=null;
         }
@@ -42,6 +118,34 @@ function studentEngine(studentListDiv){
     }
 }
 
+function studentDisplayEngine(studentListDiv){
+	var studentIcons={};
+	this.addStudent=function(uuid){
+		var studentIcon=new studentIconClass();
+		studentListDiv.appendChild(studentIcon);
+		studentIcons[uuid]=studentIcon;
+	}
+	function studentIconClass(){
+		var sDiv=document.createElement('div');
+		$(sDiv).addClass("studentIcon");
+		$(sDiv).addClass("unanswered");
+		return sDiv;
+	}
+	this.markAnswered=function(uuid){
+		$(studentIcons[uuid]).removeClass("unanswered");
+	}
+	this.resetAnswered=function(){
+        for(var uuid in studentIcons){
+    		$(studentIcons[uuid]).addClass("unanswered");
+        }
+	}
+	this.markDisconnected=function(uuid){
+		$(studentIcons[uuid]).addClass("disconnected");
+	}
+	this.markConnected=function(uuid){
+		$(studentIcons[uuid]).removeClass("disconnected");
+	}
+}
 
 function ctrlBarEngine(prevBtn,resetBtn,nextBtn){
     (function init(){
@@ -72,19 +176,17 @@ function interfaceHandler(titleDiv){
         titleDiv.innerHTML="Lesson <font class='gameId'>"+lessonId+"</font>";
     }
     this.studentDisCon=function(studentSocketId){
-        studentObj.disConStudent(studentSocketId);
+        studentIoObj.disConStudent(studentSocketId);
     }
     
     this.relay=function(packet){
-        console.log(packet);
         switch(packet.msg.title){
             case 'studentParams=':
-                studentObj.addStudent(packet.socketId,packet.msg.studentName,packet.msg.uuid);
+                studentIoObj.addStudent(packet.socketId,packet.msg.studentName,packet.msg.uuid);
                 socket.relay(packet.socketId,{"title":"qnCmd","jsFile":lessonObj.jsFile,"params":lessonObj.jsParams});
                 break;
             case 'ans':
                 lessonObj.processAns(packet.socketId,packet.msg.data);
         }
     }
-
 }
